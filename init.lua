f614769221e0616ecc64e6c71f69c6a105233d8e
Lars Mueller <appgurulars@gmx.de>
2020-07-30 00:06:21 +0200

    Code quality: use new modlib functionality

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
-cmdlib = {}
-modlib.mod.init("cmdlib")
-modlib.mod.extend("cmdlib", "override")
+modlib.mod.init()
+modlib.mod.extend("override")
 -- Tests - only uncomment if testing stuff
 --[[
 --dofile(modlib.mod.get_resource("cmdlib", "test.lua"))
